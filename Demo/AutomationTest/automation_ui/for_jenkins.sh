970ed73596c84f90a0d2d7637b43346d5c5514c8
Danielyan86 <516495459@qq.com>
2020-07-11 20:54:22 +0800

    update

---
+++ Demo/AutomationTest/automation_ui/for_jenkins.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+docker kill $(docker ps -q)
+docker container prune -f
+
+cd Demo/AutomationTest/automation_ui
+docker run -d --rm -p 5904:25900 -p 4444:24444 --name automation-container -v "$(pwd)":/home/seluser/automation -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 automation-test:latest
+sleep 20
+docker exec automation-container python automation/BingAutomationTest_start_inside_container.py
+docker container stop automation-container
