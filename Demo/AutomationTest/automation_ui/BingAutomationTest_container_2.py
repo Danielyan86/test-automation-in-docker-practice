6d1f967ab7537762aefd179596d587eac50129b5
Danielyan86 <516495459@qq.com>
2020-07-11 22:23:02 +0800

    update

---
+++ Demo/AutomationTest/automation_ui/BingAutomationTest_container_2.py
@@ -0,0 +1,39 @@
+import logging
+import time
+
+from selenium import webdriver
+
+
+def run_UI_test_in_docker_env(selenium_grid_url=None):
+    # selenium grid方式远程运行
+    current_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    logging.basicConfig(filename="./selenium_deb_log_{0}".format(current_time), level=logging.DEBUG)
+
+    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+    d = webdriver.Remote(command_executor=selenium_grid_url, desired_capabilities=capabilities)
+
+    # 设置超时
+    d.set_page_load_timeout(10)
+    d.set_script_timeout(10)
+
+    url = "http://www.bing.com"
+    text_box_id = "sb_form_q"
+    # 输入URL
+    d.get(url)
+    # 搜索框输入test
+    try:
+        d.find_element_by_id(text_box_id).send_keys("test\n")
+    except Exception as e:
+        print(e)
+    finally:
+        time.sleep(20)
+        d.close()
+        d.quit()
+
+
+if __name__ == '__main__':
+    # 运行此脚本之前需要一个已经启动起来的容器
+    # docker run -d --rm -p 5906:25900 -p 4446:24444 --name automation-container_2  -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 automation-test:latest
+    # 此脚本用于本地执行，方便调试使用
+    selenium_grid_url = "http://127.0.0.1:4446/wd/hub"
+    run_UI_test_in_docker_env(selenium_grid_url=selenium_grid_url)
--- Demo/AutomationTest/automation_ui/run_script_in_container.sh
+++ Demo/AutomationTest/automation_ui/run_script_in_container.sh
@@ -3,5 +3,5 @@ set -e
 docker container prune -f
 docker run -d --rm -p 5904:25900 -p 4444:24444 --name automation-container -v "$(pwd)":/home/seluser/automation -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 automation-test:latest
 sleep 20
-docker exec automation-container python automation/BingAutomationTest_start_in_container.py
+docker exec automation-container python automation/BingAutomationTest_start_inside_container.py
 docker container stop automation-container
