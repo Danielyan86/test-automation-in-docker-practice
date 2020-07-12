f3a0663c46b36494f69e6fed54590ab4b48c4934
Danielyan86 <516495459@qq.com>
2020-07-12 10:28:45 +0800

    update

--- Demo/AutomationTest/automation_ui/BingAutomationTest_local.py
+++ Demo/AutomationTest/automation_ui/BingAutomationTest_local.py
@@ -1,4 +1,3 @@
-import logging
 import time
 
 from selenium import webdriver
@@ -22,30 +21,5 @@ def run_UI_test_in_local_env():
     d.quit()
 
 
-def run_UI_test_in_docker_env():
-    # selenium grid方式远程运行
-    current_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-    logging.basicConfig(filename="./selenium_deb_log_{0}".format(current_time), level=logging.DEBUG)
-
-    selenium_grid_url = "http://127.0.0.1:4444/wd/hub"
-    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-    d = webdriver.Remote(command_executor=selenium_grid_url, desired_capabilities=capabilities)
-
-    url = "http://www.bing.com"
-    text_box_id = "sb_form_q"
-    # 输入URL
-    d.get(url)
-    # 搜索框输入test
-    try:
-        d.find_element_by_id(text_box_id).send_keys("test\n")
-    except Exception as e:
-        print(e)
-    finally:
-        time.sleep(10)
-        d.close()
-        d.quit()
-
-
 if __name__ == '__main__':
-    # run_UI_test_in_local_env()
-    run_UI_test_in_docker_env()
+    run_UI_test_in_local_env()
